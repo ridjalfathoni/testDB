9ea5f5f4efd3943f0232eed0bde330b723695547
ridjal_fathoni <fathonirosyidi@gmail.com>
2017-10-10 17:44:12 +0700

    Initial Commit

---
+++ app/src/main/java/com/iot/a2a/testdb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iot.a2a.testdb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
